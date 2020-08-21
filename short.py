3e34d94a6edce633b58acafee064ef1d6c6cfca9
liqap <67596640+liqap@users.noreply.github.com>
2020-08-21 08:55:53 +0300

    Update short.py

--- short.py
+++ short.py
@@ -1,5 +1,10 @@
+import pyfiglet
+from colorama import init, Fore
 import pyshorteners
 
+banner = pyfiglet.figlet_format("shorturl")
+print(Fore.GREEN + banner)
+\n\n
 url = str(input("Введите ссылку: "))
 
 print(f"Усешно! Ваша ссылка: {pyshorteners.Shortener().tinyurl.short(url)}")
