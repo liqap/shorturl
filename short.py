403c7e870975c9222775659d01653e5d6631d3e5
liqap <67596640+liqap@users.noreply.github.com>
2020-08-21 09:00:29 +0300

    Update short.py

--- short.py
+++ short.py
@@ -4,7 +4,7 @@ import pyshorteners
 
 banner = pyfiglet.figlet_format("shorturl")
 print(Fore.GREEN + banner)
-\n\n
+
 url = str(input("Введите ссылку: "))
 
 print(f"Усешно! Ваша ссылка: {pyshorteners.Shortener().tinyurl.short(url)}")
